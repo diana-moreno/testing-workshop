582215d54e0fe54c5eb98de22525a219f3a68a4a
Diana <d7@hotmail.es>
2020-05-13 10:32:58 +0200

    Add test to login-step-1

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,3 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '../login'
+
 // Basic unit test
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
@@ -10,7 +14,23 @@ test('calls onSubmit with the username and password when submitted', () => {
   // TIP: const inputs = div.querySelectorAll('input')
   // TIP: const form = div.querySelector('form')
   // fill in the field values
-  //
+
+  const handleSubmit = jest.fn()
+  const container = document.createElement('div')
+  ReactDOM.render(<Login onSubmit={handleSubmit} />, container)
+
+  const form = container.querySelector('form')
+  const {username, password} = form.elements
+  username.value = 'chucknorris'
+  password.value = 'password'
+
+  form.dispatchEvent(new window.Event('submit'))
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username.value,
+    password: password.value,
+  })
   // Act
   // submit the form:
   // TIP: formNode.dispatchEvent(new window.Event('submit'))
